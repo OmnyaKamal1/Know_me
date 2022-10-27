aafc647ce278dacf2194d8dd03d44bcf4f6acaaa
omnya <omnyakamal1@icloud.com>
2022-10-27 09:55:45 +0300

    Initial Commit

---
+++ Know_me/Know_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  Know_meApp.swift
+//  Know_me
+//
+//  Created by Omnya Kamal  on 27/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Know_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
