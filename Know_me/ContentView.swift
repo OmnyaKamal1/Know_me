6c488528a538630da54b7aaa6952f26f00fcd163
omnya <omnyakamal1@icloud.com>
2022-10-27 12:02:58 +0300

    Adding "Omnya Change" label

--- Know_me/ContentView.swift
+++ Know_me/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Image("myBackground")
+                .resizable()
+                .frame(width: 700.0, height: 1400.0)
+                .opacity(0.5)
+                
+            VStack{
+                Image("me")
+                    .resizable()
+                    .frame(width: 200.0, height: 200.0)
+                    .cornerRadius(/*@START_MENU_TOKEN@*/200.0/*@END_MENU_TOKEN@*/)
+                Text("Omnya Kamal")
+                    .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(hue: 1.0, saturation: 0.833, brightness: 0.61, opacity: 0.674))
+                Text("Iam a software engineer graduated from Prince Sultan University with first class honor degree ")
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 350.0, height: 200.0)
+                    
+                    
+                    
+                    
+             
+            }
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            
+           
         }
-        .padding()
     }
 }
 
---
+++ OmnyaChange.swift
@@ -0,0 +1,21 @@
+//
+//  OmnyaChange.swift
+//  Know_me
+//
+//  Created by Omnya Kamal  on 27/10/2022.
+//
+
+import SwiftUI
+
+struct OmnyaChange: View {
+    var body: some View {
+        Text("Change Omnya")
+            .font(.largeTitle)
+    }
+}
+
+struct OmnyaChange_Previews: PreviewProvider {
+    static var previews: some View {
+        OmnyaChange()
+    }
+}
